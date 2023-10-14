1ee0cd2076852cfdb2fd6f31f07fc03dc5ff543d
Leidy Johana <lj13.az50@gmail.com>
2023-10-14 10:50:22 -0500

    se crea verificación por 2fa

--- src/services/seguridad-usuario.service.ts
+++ src/services/seguridad-usuario.service.ts
@@ -2,27 +2,56 @@
 //se crea un  clase  servicio  para ejecutarse dentro de un conexto de aplicacion
 //este aporta a las seguridad del usuario
 import { /* inject, */ BindingScope, injectable} from '@loopback/core';
+import { Creedenciales, Usuario } from '../models';
+import { repository } from '@loopback/repository';
+import { UsuarioRepository } from '../repositories';
 const  generator = require('generate-password');
 const MD5 = require("crypto-js/md5");
 @injectable({scope: BindingScope.TRANSIENT})
 export class SeguridadUsuarioService {
-  constructor(/* Add @inject to inject parameters */) {}
+  constructor(
+ @repository(UsuarioRepository)
+ public repositorioUsuario: UsuarioRepository,
+
+
+ ) {}
 
   /*
-   * Add service methods here
+   * crear una clave aleatoria 
+   *@returns cadena aleatoria de 10 caracteres
    */
-
-  crearClave():string{
+  crearTextoAleatorio(n:number):string{
     //genera nuevas contraseñas
     let clave = generator.generate({
-      length: 10,
+      length: n,
       numbers: true
     });
     return clave;
   }
+
+  /**
+   * cifrar una cadena con metodo md5
+   * @param cadena de texto a cifrar
+   * @returns cadena cifrada con md5
+   */
   cifrarTexto(cadena:string):string{
     //cifra el texto
      let cadenaCifrado = MD5(cadena).toString();
      return cadenaCifrado;
   }
+
+ /**
+  * Se busca un usuario por las credenciales de su acceso
+  * @param credenciales credenciales del usuario
+  * @returns usuario encontrado o null
+  */
+ async identificarUsuario(credenciales:Creedenciales):Promise<Usuario | null>{
+  let usuario = await this.repositorioUsuario.findOne({
+    where:{
+    correo: credenciales.correo,
+    clave: credenciales.clave
+    }
+  });
+   return usuario as Usuario;
+ }
 }
